9610bef8ca14731789717d3c5d4348801d5af4b2
shereef.hamed <shereefhamed131@gmail.com>
2025-09-30 09:24:03 +0300

    fix cv file path

--- src/components/Resume/Dowload/Download.jsx
+++ src/components/Resume/Dowload/Download.jsx
@@ -1,9 +1,10 @@
 import styles from './Download.module.css';
+import cvFile from './assets/shereef-hamed.pdf';
 
 const Download = (props) => {
     return (
         <div className={styles.Download}>
-            <a href="/shereef-hamed.pdf" download>
+            <a href={cvFile} download>
                 <span className={styles.DownloadIcon}><i className="fa-solid fa-download"></i></span> Download Resume
             </a>
 
